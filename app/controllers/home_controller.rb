43a8120f401a9bd7c93a21e8ae8d4efdc2a4d982
SOO YANG, TAN <sooyang.code@gmail.com>
2018-05-14 22:52:26 +0800

    Add word feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,13 +8,22 @@ class HomeController < ApplicationController
   end
 
   def select
-    board = Board.new("T, A, P, *, E, A, K, S, O, B, R, S, S, *, X, D", session[:game]["selected"], session[:game]["indexes"]).selected(params["value"], params["index"])
+    board = Board.new("T, A, P, *, E, A, K, S, O, B, R, S, S, *, X, D", session[:game]["selected"], session[:game]["indexes"], session[:game]["added"]).selected(params["value"], params["index"])
     if board == "ok"
       render :json => { :word => "masquerade_word "}, :status => 200
     elsif board == "removed"
       render :json => { :word => "removed"}, :status => 200
     else
       render :json => { :word => "masquerade_word "}, :status => 400
+    end
+  end
+
+  def add
+    board = Board.new("T, A, P, *, E, A, K, S, O, B, R, S, S, *, X, D", session[:game]["selected"], session[:game]["indexes"], session[:game]["added"]).add
+    if board == "ok"
+      render :json => { :word => "correct"}, :status => 200
+    else
+      render :json => { :word => "wrong"}, :status => 400
     end
   end
 end
--- app/models/library/boggle.rb
+++ app/models/library/boggle.rb
@@ -1,9 +1,10 @@
 module Boggle
   class Board
-    def initialize(board, selected = [], indexes = [])
+    def initialize(board, selected = [], indexes = [], added = [])
       @board = board
       @selected = selected
       @indexes = indexes
+      @added = added
     end
 
     def draw
@@ -13,11 +14,12 @@ module Boggle
     def selected(value, index)
       # byebug
       if @selected.last
-        if @selected.last == value
+        if @selected.last == value && @indexes.last == index
           @selected.pop()
           @indexes.pop()
           return 'removed'
         else
+          # byebug
           if index.first.to_i.between?(@indexes.last.first.to_i - 1, @indexes.last.first.to_i + 1) && index.last.to_i.between?(@indexes.last.last.to_i - 1, @indexes.last.last.to_i + 1) && !@indexes.include?(index)
             @selected << value
             @indexes << index
@@ -31,7 +33,30 @@ module Boggle
         @indexes << index
         return "ok"
       end
-      session[:game] = self
+    end
+
+    def add
+      array = File.readlines("lib/dictionary.txt").grep(/#{@selected.join.downcase}/)
+      if @selected.include?('*')
+        new_array = array.map {|b| b if b.gsub("\n", "").length == @selected.length }.compact
+        newer_array = new_array.map {|a| a.gsub("\n", "")}
+        newest_array = newer_array.map {|x| x.split("").each_with_index.map {|b,i| b == @selected[i].downcase} }
+        latest = newest_array.map {|a| a.count(true) == @selected.count - @selected.count("*")}
+
+        if latest.count(true) > 0 && @added.count(@selected.join.downcase) < latest.count(true)
+          @added << @selected.join.downcase
+          return "ok"
+        else
+          @added << @selected.join.downcase
+          return "wrong"
+        end
+      else
+        if array.include?("#{@selected.join.downcase}\n")
+          return "ok"
+        else
+          return "wrong"
+        end
+      end
     end
   end
 end
