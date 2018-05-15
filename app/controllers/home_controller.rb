0e2d04b90f680600e091c9a888381d88a5931fcc
SOO YANG, TAN <sooyang.code@gmail.com>
2018-05-15 20:13:52 +0800

    Show word selected

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,9 +10,9 @@ class HomeController < ApplicationController
   def select
     board = Board.new("T, A, P, *, E, A, K, S, O, B, R, S, S, *, X, D", session[:game]["selected"], session[:game]["indexes"], session[:game]["added"]).selected(params["value"], params["index"])
     if board == "ok"
-      render :json => { :word => "masquerade_word "}, :status => 200
+      render :json => { :word => "masquerade_word ", :selected => session[:game]["selected"]}, :status => 200
     elsif board == "removed"
-      render :json => { :word => "removed"}, :status => 200
+      render :json => { :word => "removed", :selected => session[:game]["selected"]}, :status => 200
     else
       render :json => { :word => "masquerade_word "}, :status => 400
     end
