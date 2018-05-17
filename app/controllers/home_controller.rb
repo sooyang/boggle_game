7e301712e5039ea3f4f8812b2a4ac4aa1c5dc395
SOO YANG, TAN <sooyang.code@gmail.com>
2018-05-17 18:08:38 +0800

    Move actions to game controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,60 +1,9 @@
 class HomeController < ApplicationController
   include Boggle
 
-  before_action :set_board, only: [:index, :start]
-  before_action :set_game, only: [:start]
-  before_action :current_game, only: [:select, :add]
-
   def index
-    @board = @new_board.draw
-    new_boggle_session
-  end
-
-  def start
-    @board = @new_board.generate_board
-    new_boggle_session
-  end
-
-  def select
-    response = @current_game.select_char(params["value"], params["index"])
-    if response == "ok"
-      render json: { selected_char: session[:game]["selected_char"]}, status: 200
-    elsif response == "removed"
-      render json: { action: "removed", selected_char: session[:game]["selected_char"]}, status: 200
-    else
-      render json: { action: "error"}, status: 400
-    end
-  end
-
-  def add
-    response = @current_game.add
-    if response == "ok"
-      render :json => { action: "correct", selected_char: session[:game]["selected_char"]}, status: 200
-    elsif response == "short"
-      render :json => { action: "short", selected_char: session[:game]["selected_char"]}, status: :bad_request
-    else
-      render :json => { action: "wrong", selected_char: session[:game]["selected_char"]}, status: :bad_request
-    end
-    session[:game]["selected_char"] = []
-    session[:game]["indexes"] = []
-  end
-
-  private
-
-  def set_board
-    @new_board = Board.new
-  end
-
-  def set_game
-    @new_game = Game.new
-  end
-
-  def current_game
-    @current_game = Game.new(session[:game]["selected_char"], session[:game]["indexes"], session[:game]["words_added"])
-  end
-
-  def new_boggle_session
+    @board = Board.new.draw
     session[:board] = @board
-    session[:game] = @new_game
+    session[:game] = Game.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   get 'home' => 'home#index'
-  post 'select' => 'home#select'
-  post 'add' => 'home#add'
-  get 'start' => 'home#start'
+  post 'select' => 'game#select'
+  post 'add' => 'game#add'
+  get 'start' => 'game#start'
 end
